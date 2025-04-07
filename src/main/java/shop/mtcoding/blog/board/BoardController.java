ec0e028dda906536adb28afa6e5913b440b70470
son <sssk1755@gmail.com>
2025-04-07 16:29:02 +0900

    게시글상세보기

--- src/main/java/shop/mtcoding/blog/board/BoardController.java
+++ src/main/java/shop/mtcoding/blog/board/BoardController.java
@@ -48,13 +48,16 @@ public class BoardController {
     @GetMapping("/board/{id}")
     public String detail(@PathVariable("id") Integer id, HttpServletRequest request) {
         User sessionUser = (User) session.getAttribute("sessionUser");
-        if (sessionUser == null) {
-        }
-        BoardResponse.DetailDTO detailDTO = boardService.글상세보기(id, sessionUser.getId());
+        Integer sessionUserId = (sessionUser == null ? null : sessionUser.getId());
+        BoardResponse.DetailDTO detailDTO = boardService.글상세보기(id, sessionUserId);
         request.setAttribute("model", detailDTO);
         return "board/detail";
     }
 
+    @GetMapping("/board/{id}/update-form")
+    public String updateForm() {
+        return "update-form";
+    }
 }
 
 
